9b8b5e8e31568f62fbc83fa94e59674df804255a
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-07-26 18:31:40 -0600

    working on modal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import ImageCard from './components/ImageCard.js';
 import ImageSearch from './components/ImageSearch.js';
-import ImageModal from './components/ImageModal.js';
+// import ImageModal from './components/ImageModal.js';
 
 
 function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState('');
-
+  const [index, setIndex] = useState('');
+  const [displayModal, setDisplayModal] = useState(false);
+  const next = () => {
+    setIndex((i) => (i + 1) % images.length);
+  };
+  const prev = () => {
+    setIndex((i) => ((i - 1) % images.length) % images.length);
+  };
+  const onClickOutside = (e) => {
+    if (e.target.localName !== "button") {
+      setDisplayModal(false);
+    }
+  };
   useEffect(() => {
     fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
       .then(res => res.json())
@@ -18,16 +30,24 @@ function App() {
       })
       .catch(err => console.log(err));
   }, [term]);
+
+  useEffect(() => {
+    window.addEventListener("click", onClickOutside);
+    return () =>
+      window.removeEventListener("click", onClickOutside);
+  }, []);
   return (
     <div className="container mx-auto">
+      <button onClick={() => setDisplayModal(true)}>
       <ImageSearch searchText={(text) => setTerm(text)} />
       {!isLoading && images.length === 0 && <h1 className="text-6xl text-center mx-auto mt-32">No Images Found</h1>}
-      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">...Loading...</h1>: <div className="grid grid-cols-3 gap-4">
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">...Loading...</h1> : <div className="grid grid-cols-3 gap-4">
         {images.map(image =>
           <ImageCard key={image.id} image={image} />
           )}
       </div>}
-    </div>
+        </button>
+        </div>
   );
 }
 
--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -1,5 +1,10 @@
-import React from 'react'
-export default class ImageModal extends React.Component {
-  state = { isOpen: false };
-}
+// import { useState, useEffect } from "react";
+
+// const images = [
+//   "https://images.dog.ceo/breeds/pomeranian/n02112018_5091.jpg",
+//   "https://images.dog.ceo/breeds/mountain-swiss/n02107574_753.jpg",
+//   "https://images.dog.ceo/breeds/leonberg/n02111129_3028.jpg"
+// ];
+
+
 
