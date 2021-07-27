058f440b6822ec5e34be1d0229af415852fbb678
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-07-26 19:31:12 -0600

    still working on image modal

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState('');
-  const [index] = useState('');
+  // const [index] = useState('');
   const [displayModal, setDisplayModal] = useState(false);
 
   const onClickOutside = (e) => {
@@ -44,17 +44,17 @@ function App() {
             backgroundColor: "white"
           }}
           >
-            <img src={images[index]} alt="" style={{ width: 200, height: 200}} />
+            <img src={images.id} alt="" style={{ width: 500, height: 500}} />
            </div>
         )}
-      <ImageSearch searchText={(text) => setTerm(text)} />
-      {!isLoading && images.length === 0 && <h1 className="text-6xl text-center mx-auto mt-32">No Images Found</h1>}
+        <ImageSearch searchText={(text) => setTerm(text)} />
       {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">...Loading...</h1> : <div className="grid grid-cols-3 gap-4">
         {images.map(image =>
-          <ImageCard key={image.id} image={image} />
+          <ImageCard key={image.id} image={image} onClick={()=> setDisplayModal(true)} />
           )}
       </div>}
         </button>
+      {!isLoading && images.length === 0 && <h1 className="text-6xl text-center mx-auto mt-32">No Images Found</h1>}
     </div>
   );
 }
